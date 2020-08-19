8a4d435571d56a27f885e3ff20cc1c45a98c725a
kishyaut <kishyaut@gmail.com>
2020-08-19 03:01:22 -0400

    upload

---
+++ sample-ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/sumojmk/Documents/sample-ws/src
+# Build directory: /home/sumojmk/Documents/sample-ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
